d4e6b1e6d866280ae04e639854473ee89b56d531
jbljw02 <jbljw02@naver.com>
2023-05-28 23:52:14 +0900

    20230528 23:52

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import search from './img/search.png';
+import starOn from './img/star-on.png'
 import starOff from './img/star-off.png'
-import starOng from './img/star-on.png'
 import sort from './img/sort.png'
-
-function star_change(){
-  
-  
-}
 
 function App() {
 
   const [cr_names, setCr_names] = useState<string[]>([]);
   const [cr_price, setCr_price] = useState<string[]>([]);
+  const [bookmark_on, setBookmark_on] = useState<string>(starOn);
+  const [bookmark_off, setBookmark_off] = useState<string>(starOff);
   
   useEffect(() => {
     fetchData();
@@ -34,104 +31,134 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-        <header className='header'>
-        </header>
-
+        <Header></Header>
         <div className='center'>
           <section className='left'>
-            <article className='left-top'>
-            </article>
-            <article className='left-bottom'>
-            </article>
+            <Left_top></Left_top>
+            <Left_Bottom></Left_Bottom>
           </section>
-
           <aside className='right'>
-            <div className='div-list'>
-              {/* 검색 공간 */}
-              <div className='list-search'>
-                <input type="text" placeholder='검색'></input>
-                <button className='btn-search'>
-                  <img className='img-search' src={search}></img>
-                </button>
-              </div>
-
-              {/* 화폐 구분 목록 */}
-              <div className='list-category'>
-                <span className='krw'>
-                  원화
-                </span>
-                <span className='btc'>
-                  BTC
-                </span>
-                <span className='usdt'>
-                  USDT
-                </span>
-                <span className='favorite'>
-                  관심
-                </span>
-              </div>
-
-              <div className='list-head'>
-
-              </div>
-
-              {/* 화폐 정보 테이블 */}
-              <table className='list-table'>
-                <thead>
-                  <tr>
-                    <th className='name'>
-                      화폐명&nbsp;
-                      <img src={sort}>
-                      </img>
-                    </th>
-                    <th className='price'>
-                      현재가&nbsp;
-                      <img src={sort}>
-                      </img>
-                    </th>
-                    <th className='compare'>
-                      전일대비&nbsp;
-                      <img src={sort}>
-                      </img>
-                    </th>
-                    <th className='transaction'>
-                      거래대금&nbsp;
-                      <img src={sort}>
-                      </img>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {/* map 반복문을 이용해 <tr><td>생성, 화폐 정보 출력 */}
-                  {
-                    cr_names.map((item, i) => 
-                    <tr key={i}>
-                        <td className='td-star'>
-                          <img onClick={() => {
-                            star_change();
-                          }} src={starOff}>
-                          </img>
-                        </td>
-                        <td className='td-name'>{item} <br /> KRW-BTC</td>
-                        <td className='td-price'>{cr_price[i]}</td>
-                        <td className='td-compare'>+0.68% <br />526,000</td>
-                        <td className='td-transaction'>96,555백만</td>
-                    </tr>)
-                  }               
-                </tbody>
-              </table>
-            </div>
+            <List cr_names={cr_names} cr_price={cr_price} bookmark_on={bookmark_on} bookmark_off={bookmark_off}></List>
           </aside>
         </div>
-
-        <footer className='footer'>
-          {/* <p>{data.map((item) => item).join(', ')}</p> */}
-        </footer>
-        
+        <Footer></Footer>
       </div>
-
     </div>
   );
+}
+
+function Header() {
+  return (
+    <header className='header'>
+    </header>
+  )
+}
+
+function Left_top() {
+  return (
+    <article className='left-top'>
+    </article>
+  )
+}
+
+function Left_Bottom() {
+  return (
+    <article className='left-bottom'>
+    </article>
+  )
+}
+
+function List(props: { cr_names: string[], cr_price: string[], bookmark_on: string, bookmark_off: string }) {
+
+  function bookmark_change() {
+    console.log("변경")
+  }
+
+  return (
+    <div className='div-list'>
+
+      {/* 검색 공간 */}
+      <div className='list-search'>
+        <input type="text" placeholder='검색'></input>
+        <button className='btn-search'>
+          <img className='img-search' src={search}></img>
+        </button>
+      </div>
+
+      {/* 화폐 구분 목록 */}
+      <div className='list-category'>
+        <span className='krw'>
+          원화
+        </span>
+        <span className='btc'>
+          BTC
+        </span>
+        <span className='usdt'>
+          USDT
+        </span>
+        <span className='favorite'>
+          관심
+        </span>
+      </div>
+      <div className='list-head'>
+      </div>
+
+      {/* 화폐 정보 테이블 */}
+      <table className='list-table'>
+        <thead>
+          <tr>
+            <th className='name'>
+              화폐명&nbsp;
+              <img src={sort}>
+              </img>
+            </th>
+            <th className='price'>
+              현재가&nbsp;
+              <img src={sort}>
+              </img>
+            </th>
+            <th className='compare'>
+              전일대비&nbsp;
+              <img src={sort}>
+              </img>
+            </th>
+            <th className='transaction'>
+              거래대금&nbsp;
+              <img src={sort}>
+              </img>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {/* map 반복문을 이용해 <tr><td>생성, 화폐 정보 출력 */}
+          {
+            props.cr_names.map((item, i) =>
+              <tr key={i}>
+                <td className='td-star'>
+                  <img onClick={() => {
+                    bookmark_change();
+                  }} src={props.bookmark_off}>
+                  </img>
+                </td>
+                <td className='td-name'>{item} <br /> KRW-BTC</td>
+                <td className='td-price'>{props.cr_price[i]}</td>
+                <td className='td-compare'>+0.68% <br />526,000</td>
+                <td className='td-transaction'>96,555백만</td>
+              </tr>)
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      {/* <p>{data.map((item) => item).join(', ')}</p> */}
+    </footer>
+  )
 }
 
 export default App;
