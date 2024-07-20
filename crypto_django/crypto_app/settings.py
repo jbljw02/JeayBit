f35810cd58186dde4cc23cf493ae180db53fe355
Jinu <jbljw02@naver.com>
2024-07-20 20:49:46 +0900

    update redis setting

--- crypto_django/crypto_app/settings.py
+++ crypto_django/crypto_app/settings.py
@@ -64,7 +64,7 @@ SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 X_FRAME_OPTIONS = 'DENY'
 
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.getenv('REDIS_URL')
 
 # 결과 백엔드로는 장고 DB를 사용
 CELERY_RESULT_BACKEND = 'django-db'
