6db3c1beafadc6e4ad602286e5ed53bf20c0b4f4
Jinu <jbljw02@naver.com>
2024-07-22 20:31:13 +0900

    fix: websocket path

--- crypto_django/crypto_app/routing.py
+++ crypto_django/crypto_app/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'wss/trade_updates/$', consumers.TradeConsumer.as_asgi()),
+    re_path(r'ws/trade_updates/$', consumers.TradeConsumer.as_asgi()),
 ]
