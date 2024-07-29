1102429334236796bf19c7b8e3dd61c2cff2c16f
Jinu <jbljw02@naver.com>
2024-07-29 14:37:47 +0900

    add keep_server_awake test

--- crypto_django/crypto_app/celery.py
+++ crypto_django/crypto_app/celery.py
@@ -33,6 +33,10 @@ app.conf.beat_schedule = {
     # celery 결과 정리
     "cleanup-task-results": {
         "task": "app.tasks.periodic_cleanup_task_results",
-        "schedule": 5.0,  # 10분 마다 실행
+        "schedule": crontab(hour=0, minute=0),  # 매일 자정에 실행
+    },
+      "keep-server-awake": {
+        "task": "app.tasks.keep_server_awake",
+        "schedule": crontab(minute="*/5"),  # 5분마다 실행
     },
 }
