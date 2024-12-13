be60e9d9a34996e0fb139038a24b097cd0b4b9b0
Jinu <jbljw02@naver.com>
2024-12-14 00:10:54 +0900

    레이아웃 테스트 11

--- client/src/components/trading/child/section/BuyingSection.tsx
+++ client/src/components/trading/child/section/BuyingSection.tsx
@@ -223,112 +223,121 @@ export default function BuyingSection() {
                 // 매도 - 지정가 영역
                 bidSort === '지정가' ?
                     <CustomScrollbars>
-                        <div className="trading-contents">
-                            <TradingThead
-                                options={bidSortOptions}
-                                selectedValue={bidSort}
-                                onChange={setBidSort}
-                                label="주문구분" />
-                            <div className="trading-section">
-                                <div className="trading-row">
-                                    <div className="trading-title">주문가능</div>
-                                    <div className="trading-row-contents">
-                                        {formatWithComas(user.balance)}
-                                        <span>KRW</span>
+                        <div className="trading-scroll-wrapper">
+                            <div className="trading-contents">
+                                <TradingThead
+                                    options={bidSortOptions}
+                                    selectedValue={bidSort}
+                                    onChange={setBidSort}
+                                    label="주문구분" />
+                                <div className="trading-section">
+                                    <div className="trading-row">
+                                        <div className="trading-title">주문가능</div>
+                                        <div className="trading-row-contents">
+                                            {formatWithComas(user.balance)}
+                                            <span>KRW</span>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="trading-row">
-                                    <div className="trading-title">매수가격</div>
-                                    <div className="trading-row-contents">
-                                        <TradeInput
-                                            value={formatWithComas(buyingInputValue)}
-                                            onChange={(e) => buyingPriceChange(e.target.value)}
-                                            suffix="KRW" />
+                                    <div className="trading-row">
+                                        <div className="trading-title">매수가격</div>
+                                        <div className="trading-row-contents">
+                                            <TradeInput
+                                                value={formatWithComas(buyingInputValue)}
+                                                onChange={(e) => buyingPriceChange(e.target.value)}
+                                                suffix="KRW" />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="trading-row">
-                                    <div className="trading-title" />
-                                    <div className="trading-row-contents">
-                                        <PriceRange
-                                            rangeValue={buyingPrice}
-                                            onChange={handlePriceRange}
-                                            category="buy" />
+                                    <div className="trading-row">
+                                        <div className="trading-title" />
+                                        <div className="trading-row-contents">
+                                            <PriceRange
+                                                rangeValue={buyingPrice}
+                                                onChange={handlePriceRange}
+                                                category="buy" />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="trading-row">
-                                    <div className="trading-title">주문수량</div>
-                                    <div className="trading-row-contents">
-                                        <TradeInput
-                                            value={formatWithComas(quantityInputValue)}
-                                            onChange={(e) => orderQuantityChange(e.target.value)}
-                                            suffix={selectedCrypto.market &&
-                                                (selectedCrypto.market).slice(4)} />
+                                    <div className="trading-row">
+                                        <div className="trading-title">주문수량</div>
+                                        <div className="trading-row-contents">
+                                            <TradeInput
+                                                value={formatWithComas(quantityInputValue)}
+                                                onChange={(e) => orderQuantityChange(e.target.value)}
+                                                suffix={selectedCrypto.market &&
+                                                    (selectedCrypto.market).slice(4)} />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="trading-row">
-                                    <div className="trading-title" />
-                                    <div className="trading-row-contents">
-                                        <SelectPercentage
-                                            percentage={selectedPercentage}
-                                            onClick={selectPercentage}
-                                            category="buy" />
+                                    <div className="trading-row">
+                                        <div className="trading-title" />
+                                        <div className="trading-row-contents">
+                                            <SelectPercentage
+                                                percentage={selectedPercentage}
+                                                onClick={selectPercentage}
+                                                category="buy" />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="trading-row">
-                                    <div className="trading-title">주문총액</div>
-                                    <div className="trading-row-contents">
-                                        <TradeInput
-                                            value={formatWithComas(totalInputValue)}
-                                            onChange={(e) => totalValueChange(e.target.value)}
-                                            suffix="KRW" />
+                                    <div className="trading-row">
+                                        <div className="trading-title">주문총액</div>
+                                        <div className="trading-row-contents">
+                                            <TradeInput
+                                                value={formatWithComas(totalInputValue)}
+                                                onChange={(e) => totalValueChange(e.target.value)}
+                                                suffix="KRW" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div >
+                            <TradingFooter
+                                bidSort={bidSort}
+                                designatedSubmit={designatedSubmit}
+                                marketSubmit={marketSubmit}
+                                category="buy" />
+                        </div>
                     </CustomScrollbars> :
                     // 매도 - 시장가 영역
                     <CustomScrollbars>
-                        <div className="trading-contents">
-                            <TradingThead
-                                options={bidSortOptions}
-                                selectedValue={bidSort}
-                                onChange={setBidSort}
-                                label="주문구분" />
-                            <div className="trading-section">
-                                <div className="trading-row">
-                                    <div className="trading-title">주문가능</div>
-                                    <div className="trading-row-contents">
-                                        {formatWithComas(user.balance)}
-                                        <span>KRW</span>
+                        <div className="trading-scroll-wrapper">
+                            <div className="trading-contents">
+                                <TradingThead
+                                    options={bidSortOptions}
+                                    selectedValue={bidSort}
+                                    onChange={setBidSort}
+                                    label="주문구분" />
+                                <div className="trading-section">
+                                    <div className="trading-row">
+                                        <div className="trading-title">주문가능</div>
+                                        <div className="trading-row-contents">
+                                            {formatWithComas(user.balance)}
+                                            <span>KRW</span>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="trading-row">
-                                    <div className="trading-title">주문총액</div>
-                                    <div className="trading-row-contents">
-                                        <TradeInput
-                                            value={formatWithComas(totalInputValue)}
-                                            onChange={(e) => totalValueChange(e.target.value)}
-                                            suffix="KRW" />
+                                    <div className="trading-row">
+                                        <div className="trading-title">주문총액</div>
+                                        <div className="trading-row-contents">
+                                            <TradeInput
+                                                value={formatWithComas(totalInputValue)}
+                                                onChange={(e) => totalValueChange(e.target.value)}
+                                                suffix="KRW" />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="trading-row">
-                                    <div className="trading-title" />
-                                    <div className="trading-row-contents">
-                                        <SelectPercentage
-                                            percentage={selectedPercentage}
-                                            onClick={selectPercentage}
-                                            category="buy" />
+                                    <div className="trading-row">
+                                        <div className="trading-title" />
+                                        <div className="trading-row-contents">
+                                            <SelectPercentage
+                                                percentage={selectedPercentage}
+                                                onClick={selectPercentage}
+                                                category="buy" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <TradingFooter
+                                bidSort={bidSort}
+                                designatedSubmit={designatedSubmit}
+                                marketSubmit={marketSubmit}
+                                category="buy" />
                         </div>
                     </CustomScrollbars>
             }
-            <TradingFooter
-                bidSort={bidSort}
-                designatedSubmit={designatedSubmit}
-                marketSubmit={marketSubmit}
-                category="buy" />
         </>
     )
 }
